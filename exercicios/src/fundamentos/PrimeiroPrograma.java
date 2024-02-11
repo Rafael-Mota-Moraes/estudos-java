020fef9151a745db4b690bcb034bfba91643533d
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-11 11:49:50 -0300

    Comentários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,15 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
+		// Comentário de uma linha
+		/*
+		 * Comentário 
+		 * de
+		 * várias
+		 * linhas...
+		 */
+
+		// Uma sentença de código termina com ;
 		System.out.println("Olá mundo!");
 		System.out.println(
 			"Primeiro" +
