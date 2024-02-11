1aab50c1c1ad3d29ba547d04ec8d536898a1f0df
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-11 11:33:46 -0300

    Primeiro Programa

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		System.out.println("Olá mundo!");
+		System.out.println(
+			"Primeiro" +
+					" programa!!!"
+		);
+		
+		;
+		;
+		;
+	}
+}
