1f918f4a502fe620b6d11638c2a432dd1288f6ee
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-15 12:04:58 -0300

    operador ternário

---
+++ exercicios/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,11 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	public static void main(String[] args) {
+		double media = 5.6;
+		String resultado = media >= 7 ? "Aprovado" : "Reprovado";
+		
+		System.out.println(resultado);
+		
+	}
+}
