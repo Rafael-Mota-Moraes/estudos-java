9eeee73c3361ed4426212336c3720a5519303cec
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-15 12:01:44 -0300

    operadores unários

---
+++ exercicios/src/fundamentos/operadores/Unarios.java
@@ -0,0 +1,14 @@
+package fundamentos.operadores;
+
+public class Unarios {
+	public static void main(String[] args) {
+		int a = 1;
+		a++;
+		a--;
+		System.out.println(a);
+		System.out.println(a++);
+		System.out.println(a);
+		System.out.println(--a);
+	}
+	
+}
